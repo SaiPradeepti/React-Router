1bebc29a20d242b2445eef972f7dc465bc368f9c
Sai Pradeepti <saipradeepti14@gmail.com>
2022-01-26 03:40:35 +0530

    Adding NavLink to Courses

--- React-Router-v6/src/Components/Course.js
+++ React-Router-v6/src/Components/Course.js
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function Course() {
+  const courses = ["react", "angular", "vue", "nodejs"];
+  const randomCourse = courses[Math.floor(Math.random() * courses.length)];
   return (
     <div>
       <div className="text-center text-white">Course</div>
+      <NavLink to={`/learn/courses/${randomCourse}`}>{randomCourse}</NavLink>
+      <br />
+      <NavLink to={`/learn/courses/tests`}>tests</NavLink>
       <Outlet />
     </div>
   );
